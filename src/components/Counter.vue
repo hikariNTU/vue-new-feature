adcce942f37e93c75758eba8fa509d6fa9268242
hikarintu <18393696+hikariNTU@users.noreply.github.com>
2021-07-14 16:15:59 +0800

    feat: cool game effect counter

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,7 +1,18 @@
 <template>
-  <div class="m-8 flex flex-col flex-grow" role="application">
+  <div class="m-8 flex flex-col flex-grow font-family-digit" role="application">
     <div class="text-sm text-warm-gray-600 text-center">
-      <code class="bg-light-700 py-1 px-2 rounded inline-flex mb-1">
+      <code
+        class="
+          bg-light-700
+          pt-2
+          pb-1
+          px-4
+          inline-flex
+          mb-1
+          font-family-digit
+          animate-bounce
+        "
+      >
         Counter.vue
       </code>
       <p>Setup script with vue3</p>
@@ -12,7 +23,7 @@
         flex flex-grow
         justify-center
         items-center
-        text-9xl
+        text-7xl
         font-black
         my-4
         mx-2
@@ -23,10 +34,15 @@
     </div>
 
     <div class="flex gap-4 mt-4">
-      <button @click="addCount" class="btn-count text-gray-500">ADD</button>
+      <button @click="addCount" class="btn-count">ADD</button>
       <button
         @click="reset"
-        class="btn-count border-yellow-300 text-yellow-600"
+        class="btn-count"
+        :class="{
+          'border-yellow-300': !noReset,
+          'text-yellow-600': !noReset,
+        }"
+        :disabled="noReset"
       >
         RESET
       </button>
@@ -35,7 +51,7 @@
 </template>
 
 <script setup>
-import { ref, unref } from "vue";
+import { computed, ref, unref } from "vue";
 
 const count = ref(0);
 const addCount = () => {
@@ -44,15 +60,23 @@ const addCount = () => {
 const reset = () => {
   count.value = 0;
 };
+const noReset = computed(() => unref(count) === 0);
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap");
+.font-family-digit {
+  font-family: "Press Start 2P", cursive;
+}
+
 .btn-count {
   touch-action: manipulation;
-  @apply border-[0.3rem] border-gray-300 rounded-[0.5rem] h-[4rem] flex-grow text-2xl font-bold;
-  @apply transition tracking-wider shadow-md;
-  @apply hover:bg-light-400 select-none;
+  @apply border-[0.3rem] border-gray-300 h-[4rem] flex-grow text-xl font-bold;
+  @apply transition tracking-wider;
+  @apply select-none text-gray-500;
   @apply focus-visible:ring ring-gray-700 focus:outline-none;
+  @apply not-disabled:(hover:bg-light-400 shadow-md);
+  @apply disabled:(cursor-default bg-light-900);
   @apply active:(bg-light-800 shadow-sm);
 }
 </style>
--- src/components/ToolTag.vue
+++ src/components/ToolTag.vue
@@ -10,6 +10,7 @@ li(
 
 <style>
 .tool-tag {
+  @apply font-mono;
   @apply inline-flex bg-light-700 pr-5 py-2 m-1 ml-4 rounded-lg h-8 place-items-center;
   @apply text-gray-800 font-semibold;
   @apply before:(content absolute block ml-[-7.5px] h-5 w-5 bg-light-700);
